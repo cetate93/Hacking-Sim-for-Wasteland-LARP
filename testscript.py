cd13d73b702650b79c22dbc6c47468594f545e7a
cetate93 <cetate93@gmail.com>
2017-02-11 03:30:05 -0500

    Add files via upload

---
+++ testscript.py
@@ -0,0 +1,28 @@
+def begin():
+	print(">>>USE LONG 'DICK' JOHNSON SESSION TIMEOUT<<<")
+	print("                                             ")
+	print(">>>BYPASSING LOGIN PROTOCOL<<<")
+	print("                                             ")
+	print(">>>WARNING: INCORRECT INPUT CAN RESULT IN SYSTEM ALARM")
+	
+	choice = input("Press Enter To Continue. . .")
+	
+	if choice == " ":
+		print(">>>NOW CALIBRATING<<<")
+		second()
+def second():
+		print(">>>FOLLOW THE PROMPT ON YOUR SCREEN<<<")
+		print("     ")
+		print("The quick brown fox jumps over the lazy dog")
+		
+		choice = input(">>>")
+		
+		if second == "The quick brown fox jumps over the lazy dog":
+			third()
+		elif second =="the quick brown fox jumps over the lazy dog":
+			third()
+		else:
+			print("ERROR")
+			second()
+def third():
+	print("CONGRATULATIONS A WINNER IS YOU") 
